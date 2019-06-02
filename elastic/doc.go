ad6a166600dbbe60030c6055e41403fc090c81f2
fguby <fguby1995@gmail.com>
2019-06-02 12:36:16 +0800

    update

--- elastic/doc.go
+++ elastic/doc.go
@@ -2,9 +2,8 @@ package elastic
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/olivere/elastic"
+	"quick-es/Workflow"
 )
 
 //列出所有文档
@@ -14,11 +13,22 @@ func ListMapping(index string) error {
 	if err != nil {
 		return err
 	}
+	wf := Workflow.New()
 	for _, v := range docs[index].(map[string]interface{}) {
-		for k, v1 := range v.(map[string]interface{}) {
-			fmt.Printf("索引下的文档为%v\n", k)
-			fmt.Printf("对应的值为%v\n", v1)
+		for k, _ := range v.(map[string]interface{}) {
+			wf.Add(false, k, "......", "img/1.icns", "")
 		}
+	}
+	wf.SendFeedback()
+	return nil
+}
+
+//创建一个文档
+func CreateType(index, doc string) error {
+	mapping := elastic.NewPutMappingService(client)
+	_, err := mapping.Index(index).Type(doc).Do(context.Background())
+	if err != nil {
+		return err
 	}
 	return nil
 }
